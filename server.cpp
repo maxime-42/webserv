952cd85083b1a4ac706882fd4c8b5c719f70e2ac
maxime-42 <mkayumba@student.42.fr>
2021-09-18 19:00:28 +0200

    adding good indenting

--- server.cpp
+++ server.cpp
@@ -3,40 +3,42 @@
 
 namespace ft
 {
-    void server::               test_error(int error_code, char const *msg)
-    {
-        if (error_code < 0)
-        {
-            std::cout << msg << std::endl;
-            exit (ERROR);
-        }
-    }
+	void server::               test_error(int error_code, char const *msg)
+	{
+		if (error_code < 0)
+		{
+			std::cout << msg << std::endl;
+			exit (ERROR);
+		}
+	}
 
-    void server::               create_network_socket()
-    {
-        _network_socket = socket(AF_INET, SOCK_STREAM, 0);
-        test_error(_network_socket, "failled create socket _network_socket_");
-    }
+	void server::               create_network_socket()
+	{
+		_network_socket = socket(AF_INET, SOCK_STREAM, 0);
+		test_error(_network_socket, "failled create socket _network_socket_");
+	}
     
-    server::                    server(){std::cout << "constructor calling...\n" << std::endl;}
+	server::                    server(){std::cout << "constructor calling...\n" << std::endl;}
 
-     server::                   server(int port, int nb_pending):_port(port), _nb_pending(nb_pending){}
+	server::                   server(int port, int nb_pending):_port(port), _nb_pending(nb_pending){}
 
-    void server::               init_struct_sockaddr()
-    {
-        _address.sin_family = AF_INET;
-        _address.sin_addr.s_addr = INADDR_ANY;
-        _address.sin_port = htons(_port);
-        memset(_address.sin_zero, '\0', sizeof (_address.sin_zero));
-    }
+	void server::               init_struct_sockaddr()
+	{
+		_address.sin_family = AF_INET;
+		_address.sin_addr.s_addr = INADDR_ANY;
+		_address.sin_port = htons(_port);
+		 memset(_address.sin_zero, '\0', sizeof (_address.sin_zero));
+	}
 
 
-    void    server::            bind_and_listen()
-    {
-        _addrlen = sizeof(_address);
-        int ret = bind(_network_socket, (struct sockaddr *)&_address, _addrlen);
-        test_error(ret, "error while the bind");
-        _listen_fd = listen(_network_socket, _nb_pending);
-        test_error(_listen_fd, "error while the listen");
-    }
+	void    server::            bind_and_listen()
+	{
+		int ret;
+
+		_addrlen = sizeof(_address);
+		ret = bind(_network_socket, (struct sockaddr *)&_address, _addrlen);
+		test_error(ret, "error while the bind");
+		_listen_fd = listen(_network_socket, _nb_pending);
+		test_error(_listen_fd, "error while the listening");
+	}
 }
--- server_class.hpp
+++ server_class.hpp
@@ -6,36 +6,33 @@
 #include <netinet/in.h>
 
 #define ERROR -1
-#define PORT 80
 
 
 namespace ft
 {
-    class server
-    {
-    private:
-        int                 _network_socket;
-        int                 _listen_fd;
-        struct  sockaddr_in _address;
-        int                 _addrlen;
-        int                 _nb_pending;
-        int                 _port;
-        struct pollfd       _tab_fd[200];
+	class server
+	{
+	private:
+		int			_network_socket;
+		int			_listen_fd;
+		struct  sockaddr_in	_address;
+		int			_addrlen;
+		int			_nb_pending;
+		int			_port;
+		struct pollfd		_tab_fd[200];
 
-        void                init_struct_sockaddr();
 
-        /* data */
-    public:
-        void                create_network_socket();
-        void                listen_fd();   
-        server(/* args */);
-        server(int port, int nb_pending);
 
-        ~server();
-        void                 test_error(int error_code, char const *msg);
+	public:
+		server();
+		server(int port, int nb_pending);
+		~server();
+		void			create_network_socket();
+		void			listen_fd();   
+		void			test_error(int error_code, char const *msg);
+		void			bind_and_listen();
 
-        void    bind_and_listen();
-
+	private:	  
+		void			init_struct_sockaddr();
     };
- 
 }
